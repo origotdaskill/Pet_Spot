fecbab2d45d0490a3d057a229493098f01665418
origotdaskill <mr.walid.ori@gmaill.com>
2024-07-08 20:13:42 +0100

    secon ori commit

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:10000/assets/${image}`}
+        src={`https://petspot-backend-walids-projects-fc600ccd.vercel.app//assets/${image}`}
       />
     </Box>
   );
--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:10000/users/${userId}/friends`,
+      `https://petspot-backend-walids-projects-fc600ccd.vercel.app//users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
